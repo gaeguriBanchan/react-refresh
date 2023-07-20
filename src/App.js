a6c41746a095e5b32f6472392eec35cfce4c5c11
gaeguriBanchan <rlawowls93@gamil.com>
2023-07-20 14:58:27 +0900

    styled-components 2

--- src/App.js
+++ src/App.js
@@ -13,18 +13,39 @@ const Circle = styled(Box)`
   border-radius: 50px;
 `;
 
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+`;
+
 const Text = styled.span`
   color: white;
 `;
 
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
+  background-color: greenyellow;
+`;
+
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal">
-        <Text>hello</Text>
-      </Box>
-      <Circle bgColor="green"></Circle>
-    </Father>
+    <>
+      <Father>
+        <Box bgColor="teal">
+          <Text>hello</Text>
+        </Box>
+        <Circle bgColor="green"></Circle>
+        <Btn>login</Btn>
+        <Btn as="a" href="/">
+          Login
+        </Btn>
+      </Father>
+      <Input></Input>
+      <Input></Input>
+      <Input></Input>
+      <Input></Input>
+    </>
   );
 }
 
